e2f211a09857d50b6df572788349025db51f693d
Uri <uriaeri@gmail.com>
2023-09-16 12:45:03 +0200

    initial commit

---
+++ iTunesSearch/iTunesSearch/iTunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchApp.swift
+//  iTunesSearch
+//
+//  Created by Uri on 16/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
