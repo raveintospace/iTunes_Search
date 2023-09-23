56c19967a4f50899e29a79b3cde8153d8102298b
Uri <uriaeri@gmail.com>
2023-09-23 12:05:17 +0200

    AlbumDetailView creation

---
+++ iTunesSearch/iTunesSearch/Views/Album/AlbumDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumDetailView.swift
+//  iTunesSearch
+//
+//  Created by Uri on 23/9/23.
+//
+
+import SwiftUI
+
+struct AlbumDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumDetailView()
+    }
+}
