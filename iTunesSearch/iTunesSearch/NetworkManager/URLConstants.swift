adeccfd473357239388d8640d0270d0e660b917b
Uri <uriaeri@gmail.com>
2023-09-30 13:42:55 +0200

    URLConstants struct

---
+++ iTunesSearch/iTunesSearch/NetworkManager/URLConstants.swift
@@ -0,0 +1,13 @@
+//
+//  URLConstants.swift
+//  iTunesSearch
+//
+//  Created by Uri on 30/9/23.
+//
+
+import Foundation
+
+struct URLConstants {
+    let baseURL = "https://itunes.apple.com/search"
+    let detailURL = "https://itunes.apple.com/lookup"
+}
