ebe7c535d013f2d3a6dec30b3e7ae9320dd09a35
Uri <uriaeri@gmail.com>
2023-10-01 12:32:00 +0200

    first iteration movieDetail last section

--- iTunesSearch/iTunesSearch/Views/Movie/MovieDetailView.swift
+++ iTunesSearch/iTunesSearch/Views/Movie/MovieDetailView.swift
@@ -28,9 +28,48 @@ struct MovieDetailView: View {
             }
             .padding(.horizontal)
             Divider()
+            VStack(alignment: .leading) {
+                Text("Information")
+                    .font(.title3)
+                    .bold()
+                    .padding(.bottom, 2)
+                HStack() {
+                    VStack {
+                        Text("Genre")
+                    }
+                    VStack {
+                        Text(movie.primaryGenreName)
+                    }
+                }
+            }
         }
     }
 }
+
+/*
+ Grid(horizontalSpacing: 20) {
+     ForEach(songs) { song in
+         GridRow {
+             Text("\(song.trackNumber)")
+                 .font(.footnote)
+                 .gridColumnAlignment(.trailing)
+             Text(song.trackName)
+                 .gridColumnAlignment(.leading)
+             Spacer()
+             Text(formattedDuration(time: song.trackTimeMillis))
+                 .font(.footnote)
+             
+             BuySongButton(urlString: song.previewURL,
+                           price: song.trackPrice,
+                           currency: song.currency)
+         }
+         .foregroundColor(song == selectedSong ? Color.accentColor : Color(.label))
+         .id(song.trackNumber)
+         Divider()
+     }
+ }
+ .padding([.vertical, .leading])
+ */
 
 struct MovieDetailView_Previews: PreviewProvider {
     static var previews: some View {
